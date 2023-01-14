7712e241e1b2afd93124e50459e6500e28c823b4
Chrystian Martins <chrystianmsc@gmail.com>
2023-01-14 13:48:08 -0300

    Start game button, win/lose, display cards and sum

--- script/index.js
+++ script/index.js
@@ -1,16 +1,33 @@
 let firstCard = Math.floor(Math.random() * (11 - 2 + 1) + 2);
 let secondCard = Math.floor(Math.random() * (11 - 2 + 1) + 2);
+let newCard = 0
 let sum = firstCard + secondCard
+
 let hasBlackJack = false
 let isAlive = true
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
-} else if (sum > 21) {
-    console.log("You're out of the game!")
-    hasBlackJack = true
-} else {
-    console.log("Wohoo! You've got Black Jack!")
-    isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame(){
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum > 21) {
+        message = "You're out of the game!"
+        hasBlackJack = true
+        isAlive = false
+    } else {
+        message = "Wohoo! You've got Black Jack!"
+    }
+    messageEl.textContent = message
 }
 
+function drawCard(){
+    newCard = Math.floor(Math.random() * (11 - 2 + 1) + 2);
+    console.log(newCard)
+}
