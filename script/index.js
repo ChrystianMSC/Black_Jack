6e78a5152f1d5f37bff06dc146272287dba933ba
Chrystian Martins <chrystianmsc@gmail.com>
2023-01-27 18:00:20 -0300

    Refactored code and end game logic

--- script/index.js
+++ script/index.js
@@ -1,11 +1,9 @@
-let firstCard = randomCard();
-let secondCard = randomCard();
-let cards = [firstCard, secondCard]
+let cards = []
 let card = 0
-let sum = firstCard + secondCard
+let sum = 0
 
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 
 let message = ""
 let messageEl = document.getElementById("message-el")
@@ -13,7 +11,21 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "You",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function startGame() {
+    isAlive = true
+    console.log(isAlive)
+    let firstCard = randomCard();
+    let secondCard = randomCard();  
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -30,19 +42,21 @@ function renderGame(){
         message = "Do you want to draw a new card?"
     } else if (sum > 21) {
         message = "You're out of the game!"
-        hasBlackJack = true
         isAlive = false
     } else {
+        hasBlackJack = true
         message = "Wohoo! You've got Black Jack!"
     }
     messageEl.textContent = message
 }
 
 function drawCard(){
-    card = randomCard();
-    cards.push(card)
-    sum += card
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+        card = randomCard();
+        cards.push(card)
+        sum += card
+        renderGame()
+    }
 }
 
 
